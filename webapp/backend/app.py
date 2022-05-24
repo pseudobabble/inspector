e783056b62b6f24f1c9cacb4310130f8f0dd73cb
tjweldon <tomd90@gmail.com>
2022-05-24 23:20:37 +0100

    formatting

--- webapp/backend/app.py
+++ webapp/backend/app.py
@@ -10,14 +10,18 @@ repository.create_all()
 app = Flask(__name__)
 api = Api(app)
 
+api.add_resource(Documents, *Documents.routes)
+api.add_resource(
+    Documents, "/documents/get_by_ids", endpoint="get_by_ids", methods=["GET"]
+)
 api.add_resource(
     Documents,
-    *Documents.routes
+    "/documents/update_ml_documents",
+    endpoint="update_ml_documents",
+    methods=["PATCH"],
 )
-api.add_resource(Documents, "/documents/get_by_ids", endpoint="get_by_ids", methods=['GET'])
-api.add_resource(Documents, "/documents/update_ml_documents", endpoint="update_ml_documents", methods=['PATCH'])
 api.add_resource(Upload, *Upload.routes)
-#api.add_resource(Trigger, *Trigger.routes)
+# api.add_resource(Trigger, *Trigger.routes)
 
 
 _env = lambda init, key, default=None: init(os.getenv(key, default))
@@ -30,9 +34,9 @@ A convenice function for expressing typed env config consistently
 """
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(
         host=_env(str, "FLASK_HOST", default="webapp"),
-        port=_env(int,"FLASK_PORT", default="8080"),
-        debug=_env(bool,"FLASK_DEBUG", default=False),
+        port=_env(int, "FLASK_PORT", default="8080"),
+        debug=_env(bool, "FLASK_DEBUG", default=False),
     )
