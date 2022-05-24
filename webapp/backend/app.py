2752ffeba3956e6feeb1419896e704c21d7284d4
tjweldon <tomd90@gmail.com>
2022-05-24 23:31:30 +0100

    Added missing return doc

--- webapp/backend/app.py
+++ webapp/backend/app.py
@@ -31,6 +31,7 @@ A convenice function for expressing typed env config consistently
 :param init: Callable, The constructor (usually type) of the env var.
 :param key: str, The name of the env var.
 :param default: The value to return if the env var is not set.
+:return: The environment variable initialised to the desired type.
 """
 
 
