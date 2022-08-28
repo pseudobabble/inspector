72cadbd0b70b0c30aec5591e1299e212388f3072
pseudobabble <harryjohnson500@gmail.com>
2022-08-28 18:23:26 +0100

    fixed job execution config

--- webapp/document_processing/job_execution/convert_files.py
+++ webapp/document_processing/job_execution/convert_files.py
@@ -1,23 +1,41 @@
 from typing import Set
 
 
-def job_execution(file_keys: Set[str]) -> dict:
+def convert_files_to_text(
+        file_keys: Set[str],
+        blob_client_url: str,
+        access_key: str,
+        secret_key: str,
+        tika_host: str,
+        tika_port: str,
+        tika_endpoint: str
+) -> dict:
     return {
         "job_name": "convert_files_to_text",
         "repository_location_name": "dagster-ucd-document-processing",
         "repository_name": "__repository__convert_files_to_text",
         "run_config": {
-            "ops": {"get_file_keys": {"config": {"keys": [*file_keys]}}},
+            "ops": {
+                "get_file_keys": {
+                    "config": {
+                        "keys": [*file_keys]
+                    }
+                }
+            },
             "resources": {
                 "blob_client": {
                     "config": {
-                        "url": ""
+                        "url": blob_client_url
+                        "bucket_name": "files",
+                        "access_key": access_key,
+                        "secret_key": secret_key
                     }
                 },
                 "tika_client": {
                     "config": {
-                        "host": "tika",
-                        "port": "9998"
+                        "host": tika_host,
+                        "port": tika_port,
+                        "endpoint": tika_endpoint
                     }
                 },
             },
